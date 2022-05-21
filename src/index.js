4d50af909a621c0b01845c5dc10a9e7768b380bf
ishimweeli <ishimweeli0@gmail.com>
2022-05-21 13:12:38 -0500

    connecting to db
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import express from 'express'
-import * as babel from "@babel/core";
-import swaggerDocs from '../public/api-docs/swagger.js';
-import 'dotenv/config'
-const app=express()
+import babel  from "@babel/core";
+import swaggerDocs from './public/api-docs/swagger.js';
+import db from "./database/models/index.js"
+import dotenv from 'dotenv'
 
+dotenv.config()
+ 
+const app=express()
 const port = process.env.PORT || 3000;
 
 swaggerDocs(app, port);
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log("connected to the db");
+    })
+    .catch((err) => {
+        console.log("Error connecting to the db", err);
+    });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
