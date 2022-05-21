1880a81281715f3bfb5bacddd6691dcf2f80910d
ishimweeli <ishimweeli0@gmail.com>
2022-05-21 13:12:38 -0500

    connecting to db
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase
    
    Create swagger.js
    
    connecting to db
    
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase
    
    mmm
    
    ft-connecting-to-db-182201508

---
+++ src/database/migrations/20220522141813-create-user.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('../models/user.js', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('User');
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import express from 'express'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+import express from 'express';
+
+import swaggerDocs from './public/api-docs/swagger.js';
+import db from "./database/models/index.js";
+
+import passport from 'passport'
 import * as babel from "@babel/core";
-import swaggerDocs from '../public/api-docs/swagger.js';
+
 import 'dotenv/config'
+
 const app=express()
 
 const port = process.env.PORT || 3000;
 
 swaggerDocs(app, port);
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log("connected to the db");
+    })
+    .catch((err) => {
+        console.log("Error connecting to the db", err);
+    });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
