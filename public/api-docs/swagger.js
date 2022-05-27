f47daae4a98f357e5d8c932243e379d8c559c482
ishimweeli <ishimweeli0@gmail.com>
2022-05-27 23:00:05 +0200

    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    latest
    
    connecting to db
    
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase
    
    Create swagger.js
    
    connecting to db
    
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase
    
    mmm
    
    ft-connecting-to-db-182201508
    
    connecting to db
    
    connecting to db revised version
    
    connecting -to-db-second-update
    
    ft-connecting-to-db-182201508
    
    rebase
    
    Create swagger.js
    
    connecting to db
    
    chore(setting-up-db): Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB
    
    chore(setting-db) : Configure BareFoot Nomad to use PostgreSQL as DB
    
    chore(setting-db) : Configure BareFochore(setting-up-db): Configure BareFoot Nomad to use PostgreSQL as DB Engine and Sequelize ORM

--- public/api-docs/swagger.js
+++ public/api-docs/swagger.js
@@ -1,5 +1,5 @@
-import swaggerJSDoc from 'swagger-jsdoc'
-import swaggerUI from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 const options = {
   definition: {
@@ -7,8 +7,7 @@ const options = {
     info: {
       title: 'Barefoot Nomad API',
       version: '1.0.0',
-      description:
-        'Barefoot Nomad REST API For Barefoot Nomad App\n This API Will Manage:\n 1. CRUD Operations\n 2. User Authentication & Authorisation',
+      description: 'Barefoot Nomad REST API For Barefoot Nomad App\n This API Will Manage:\n 1. CRUD Operations\n 2. User Authentication & Authorisation'
     },
 
     paths: {},
@@ -24,24 +23,28 @@ const options = {
           scheme: 'bearer',
           bearerFormat: 'JWT',
           name: 'bearerAuth',
-          in: 'header',
-        },
-      },
+          in: 'header'
+        }
+      }
     },
   },
-  apis: ['./**/*.yaml'],
-}
+  apis: ['./**/*.yaml']
+};
 
-const swaggerSpec = swaggerJSDoc(options)
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app) => {
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+ 
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
+  
   app.get('/api-docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.send(swaggerSpec)
-  })
-}
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+
+};
 
 export default swaggerDocs;
 
