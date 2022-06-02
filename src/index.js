559b2bf8abd7327431f5fde153f5a29350ed62cc
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-06-02 10:58:33 +0200

    ft(login-with-facebook-google)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import express from 'express'
-import * as babel from '@babel/core'
-import swaggerDocs from '../public/api-docs/swagger.js'
-import 'dotenv/config'
-const app = express()
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import * as babel from '@babel/core';
+import swaggerDocs from '../public/api-docs/swagger.js';
+import 'dotenv/config';
 
-const port = process.env.PORT || 3000
+const app = express();
 
-swaggerDocs(app, port)
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on ${port}`))
+swaggerDocs(app, port);
+
+app.listen(port, () => console.log(`Listening on ${port}`));
