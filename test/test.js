2bc1c96c61479e77aa06b7726db4d0a75088daa2
uwishema11 <99412054+uwishema11@users.noreply.github.com>
2022-06-02 12:38:41 +0200

    set up unit testing

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ import { expect } from 'chai';
 
 describe("Simple test",()=>{
     it('Should add two numbers', done => {
-        const number = 1 + 4
-        expect(number).to.equal(5);
+        const number = 1 + 5
+        expect(number).to.equal(6);
         done();
     })
 })
