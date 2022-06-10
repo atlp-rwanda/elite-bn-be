d00c48a8f20d5246697cbaa34cab27db4bb98c91
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-06-10 14:06:09 +0200

    feature(Google-Facebook-Login): users can login in to Barefoot application via Facebook and Google

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-import express from "express";
-import bodyparser from "body-parser";
-import babel from "@babel/core";
 import swaggerDocs from '../public/api-docs/swagger.js';
 import { sequelize } from "./database/models"
 import dotenv from 'dotenv'
 import app from './app'
-import { db } from "./database/models/index.js"
-import methodoverride from "method-override";
-import cors from "cors";
 import path from "path";
 import passport from "passport";
-import morgan from "morgan";
-import cookieParser from 'cookie-parser';
-import googleFacebook from "./routes/api/googleFacebook";
+
 
 require("./config/passport").default(passport);
 
 dotenv.config();
 
 const __dirname = path.resolve();
-
-
-// app.use(cookieParser());
-// app.use(bodyparser.json());
-// app.use(cors());
-// app.use(morgan("dev"));
-// app.use(bodyparser.urlencoded({ extended: false }));
-// app.use(methodoverride());
-// const session = require('express-session');
-// const FileStore = require('session-file-store')(session);
-// const fileStoreOptions = {};
-
-// app.use('/api/v1', googleFacebook)
-
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 
  const port =  3000;
@@ -53,15 +28,9 @@ const __dirname = path.resolve();
      
      
     
-app.listen(port, () => console.log(`\nBarefoot Nomad Server Started & Listening on PORT: ${port}\n`));
+app.listen(port, () => console.log(`\nBarefoot Nomad Server Started & Listening on PORT: ${port}`));
  
 
-// app.use(session({
-//   store: new FileStore(fileStoreOptions),
-//   resave: true,
-//   saveUninitialized: false,
-//   secret: 'keyboard cat'
-// }));
 
 
 app.use((req, res, next) => {
