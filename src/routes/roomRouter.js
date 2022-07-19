ecb166ed41c262098a9d19d16d27c56ed10f8d13
KALISA_NGABO_KEVIN <kevinkalisa84@gmail.com>
2022-07-19 11:18:54 +0200

    coveralls

--- src/routes/roomRouter.js
+++ src/routes/roomRouter.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import roomController from '../controllers/room';
 import roomMiddleware from '../middlewares/room';
-import { validate } from '../middlewares';
 import checkAuth from '../middlewares/checkAuth';
 import verifyTravelAdmin from '../middlewares/travelAdminAuth';
-import { bookingSchema, checkInorCheckoutSchema } from '../validations/bookingValidation';
 
 const roomRouter = express.Router();
 const { validateNewRoom, validateRoomUpdate } = roomMiddleware;
@@ -29,37 +27,6 @@ roomRouter.delete(
   checkAuth,
   verifyTravelAdmin,
   roomController.deleteRoom
-);
-
-roomRouter.post(
-  '/v1/room/bookings/new',
-  checkAuth,
-  // validate(bookingSchema),
-  roomController.bookRoom
-);
-
-roomRouter.get('/v1/rooms/bookings', checkAuth, roomController.getAllBookings);
-
-roomRouter.delete(
-  '/v1/rooms/bookings/cancel/:bookingId',
-  checkAuth,
-  roomController.removeRoomBooking
-);
-
-roomRouter.patch(
-  '/v1/rooms/bookings/checkInOrCheckOut/:bookingId',
-  checkAuth,
-  verifyTravelAdmin,
-  // validate(checkInorCheckoutSchema),
-  roomController.roomCheckInOrCheckOut
-);
-
-roomRouter.delete(
-  '/v1/rooms/bookings/delete/:bookingId',
-  checkAuth,
-  verifyTravelAdmin,
-
-  roomController.deleteRoomBooking
 );
 
 export default roomRouter;
