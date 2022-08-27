ad04aac1b719d183bc1e0c9f55b943b80543b7c4
Munyaneza Armel <munyaarmel61@gmail.com>
2022-08-27 15:13:22 +0000

    fix(cors):fix cors so that axios can send cookies

--- src/app.js
+++ src/app.js
@@ -12,7 +12,12 @@ const app = express();
 app.use('/chats', express.static(path.join(__dirname, '../public/chat')));
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+    
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}
+    ));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(Router);
